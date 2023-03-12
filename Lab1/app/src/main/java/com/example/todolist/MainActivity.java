ef63b3c0af93a717c5bf55fb3ff25688cd20e0fc
Nikita <nickpelmog2003@gmail.com>
2023-03-12 19:01:42 +0300

    feat: configure default app

---
+++ Lab1/app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.todolist;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.appcompat.widget.SearchView;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+import com.bsuir.todolist.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity{
+
+
+}
